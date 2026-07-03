dec8faf55c52178821abfa106ba680d0b073dcd3
2ru17 <neilgutangjr@gmail.com>
2026-07-03 22:51:47 +0800

    feat: configure GitHub Actions for deployment to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,11 @@ import icon from "astro-icon";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  integrations: [mdx(), react(), icon()],
-  site: "https://2ru17.github.io/virtual-exhibit-proj-2026-g1",
-  base: "/virtual-exhibit-proj-2026-g1/",
+    integrations: [mdx(), react(), icon()],
+    site: "https://2ru17.github.io",
+    base: "/virtual-exhibit-proj-2026-g1/",
 
-  vite: {
-    plugins: [tailwindcss()],
-  },
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
